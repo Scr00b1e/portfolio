633912fbabbeb0363f8ce7c162f3c7cc750eaced
Jack <kbolot312@gmail.com>
2023-05-03 18:54:26 +0600

    added neccary files

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/header.scss'
+
+const Header: React.FC = () => {
+    return (
+        <div className='header'>
+            Scr00b1e
+        </div>
+    )
+}
+
+export default Header
