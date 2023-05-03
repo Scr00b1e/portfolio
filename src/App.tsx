c693f435f530b9ce591d4c7a4848fe2cf1899a47
Jack <kbolot312@gmail.com>
2023-05-03 19:09:19 +0600

    added intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header"
+import Intro from "./components/Intro"
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <div className="main">
+        <Intro />
+      </div>
     </div>
   )
 }
---
+++ src/components/Intro.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/intro.scss'
+
+const Intro: React.FC = () => {
+    return (
+        <div className='intro'>
+            <h1>Hi! I'm Jack,</h1>
+            <h1>a.k.a Scr00b1e</h1>
+            {/* <p>Junior Front-end developer</p> */}
+            <button>Some Button</button>
+        </div>
+    )
+}
+
+export default Intro
