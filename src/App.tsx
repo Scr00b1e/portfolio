4c61603412640013eaffa28e08c8e437bd8e9095
Jack <kbolot312@gmail.com>
2023-05-04 21:12:02 +0600

    scelet of sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -2,49 +2,53 @@ import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Intro from "./components/Intro"
 import LangsItem from "./components/LangsItem"
+import Sidebar from "./components/Sidebar"
 import { languages } from "./data/languages"
 
 function App() {
   return (
-    <div className="container">
-      <Header />
-      <div className="main">
-        <Intro />
+    <>
+      <Sidebar />
+      <div className="mini-container">
+        <Header />
+        <div className="main">
+          <Intro />
 
-        <section>
-          <div>
-            <h1>Sup, how ya doin</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas, iste magnam animi vitae vero minus deserunt aperiam repellendus, ipsum id modi doloribus voluptatem ratione assumenda, earum asperiores tenetur fugiat esse.</p>
-          </div>
-        </section>
-
-        <section>
-          <div>
-            <h1>Languages</h1>
-            <div className="flex">
-              {
-                languages.map((obj, i) => (
-                  <LangsItem {...obj} key={i} />
-                ))
-              }
+          <section>
+            <div>
+              <h1>Sup, how ya doin</h1>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas, iste magnam animi vitae vero minus deserunt aperiam repellendus, ipsum id modi doloribus voluptatem ratione assumenda, earum asperiores tenetur fugiat esse.</p>
             </div>
-          </div>
-          <div>
-            experience stuff
-          </div>
-        </section>
+          </section>
 
-        <section>
-          projs
-        </section>
+          <section>
+            <div>
+              <h1>Languages</h1>
+              <div className="flex">
+                {
+                  languages.map((obj, i) => (
+                    <LangsItem {...obj} key={i} />
+                  ))
+                }
+              </div>
+            </div>
+            <div>
+              experience stuff
+            </div>
+          </section>
 
-        <section>
-          contact me bruh
-        </section>
+          <section>
+            projs
+          </section>
 
+          <section>
+            contact me bruh
+          </section>
+
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   )
 }
 
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/sidebar.scss'
+
+const Sidebar: React.FC = () => {
+    return (
+        <aside className='sidebar'>
+            <p>hey</p>
+            <p>woow</p>
+        </aside>
+    )
+}
+
+export default Sidebar
