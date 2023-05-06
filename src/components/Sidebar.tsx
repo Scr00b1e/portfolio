b7dc376477bbd53700ffdd45637b741270388c1e
Jack <kbolot312@gmail.com>
2023-05-06 20:35:58 +0600

    sidebar changed

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,8 +4,15 @@ import '../styles/sidebar.scss'
 const Sidebar: React.FC = () => {
     return (
         <aside className='sidebar'>
-            <p>hey</p>
-            <p>woow</p>
+            <div className='flex flex__between'>
+                <div>
+                    <p>hey</p>
+                    <p>woow</p>
+                </div>
+                <div>
+                    some
+                </div>
+            </div>
         </aside>
     )
 }
