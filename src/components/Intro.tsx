7a5dc2b452e5c0d0dbaaea56bd8430e2e51f9769
Jack <kbolot312@gmail.com>
2023-05-10 21:59:04 +0600

    added ui for intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -3,11 +3,16 @@ import '../styles/intro.scss'
 
 const Intro: React.FC = () => {
     return (
-        <div className='intro'>
-            <h1>Hi! I'm Jack,</h1>
-            <h1>a.k.a Scr00b1e</h1>
-            {/* <p>Junior Front-end developer</p> */}
-            <button className='btn'>Some Button</button>
+        <div className='intro shadow'>
+            <div className='intro__info'>
+                <h1>Hi! I'm Jack,</h1>
+                <h1>a.k.a Scr00b1e</h1>
+                {/* <p>Junior Front-end developer</p> */}
+                <button className='btn'>Some Button</button>
+            </div>
+            <div>
+                <img src="./v2.png" alt="" className='intro__img' />
+            </div>
         </div>
     )
 }
