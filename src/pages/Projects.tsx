4d1dff57662f00bd7b33b667fabe129d208adc61
Jack <kbolot312@gmail.com>
2023-05-10 22:43:02 +0600

    projects page changed

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { projects } from '../data/projects'
+import ProjItem from '../components/ProjItem'
+import { Link } from 'react-router-dom'
+import '../styles/projects.scss'
 
 const Projects: React.FC = () => {
     return (
-        <div>
-            Projects
-        </div>
+        <section className="full txtCenter">
+            <h1>Projects</h1>
+            <div className="flex flex__wrap">
+                {
+                    projects.map((obj, i) => (
+                        <ProjItem {...obj} key={i} />
+                    ))
+                }
+            </div>
+            <Link to={'/'}>
+                <button className='btn'>Back</button>
+            </Link>
+        </section>
     )
 }
 
