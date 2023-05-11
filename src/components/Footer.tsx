2291436aeb7dd426952e6ad608cf7be53dffb93a
Jack <kbolot312@gmail.com>
2023-05-11 22:15:17 +0600

    footer done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ import '../styles/footer.scss'
 
 const Footer: React.FC = () => {
     return (
-        <div className='footer'>
-            Footer
+        <div className='footer txtCenter'>
+            <h3 className='footer__text'>Personal portfolio made by @scr00b1e</h3>
         </div>
     )
 }
